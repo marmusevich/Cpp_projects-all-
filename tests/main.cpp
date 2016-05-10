1973a18139786b0a085930c893df2a0871aeb9f1
marmusevich <myrmik@gmail.com>
2016-05-10 09:54:18 +0300

    -

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 using namespace std;
 
-template<class T> void kill(T *& op)
-{
-    delete op;
-}
-class m{};
 
 
 int main()
 {
-    m *mp = new m();
-    kill(mp);
-    //kill(mp);
 
     return 0;
 }
