5d236829ca3171429c526410f4a5196750267ce6
marmusevich <myrmik@gmail.com>
2016-06-30 14:01:47 +0300

    wx

---
+++ wx_test/wx_testApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      wx_testApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2016-06-30
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef WX_TESTAPP_H
+#define WX_TESTAPP_H
+
+#include <wx/app.h>
+
+class wx_testApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // WX_TESTAPP_H
