882574a5dc6bc1c7339a520081440d1019fbf89b
marmusevich <myrmik@gmail.com>
2017-06-19 23:12:30 +0300

    work closed

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+//#include <stdio.h>
+
 using namespace std;
+
+class A
+{
+public:
+    A()
+    {
+        cout <<"ctor A " <<endl;
+    }
+    ~A()
+    {
+        cout <<"dtor A " <<endl;
+    }
+};
+
+
+class B
+{
+public:
+    B()
+    {
+        cout <<"ctor B " <<endl;
+    }
+    ~B()
+    {
+        cout <<"dtor B " <<endl;
+    }
+};
+
+class C
+{
+public:
+    A a;
+    B b;
+};
 
 
 
 int main()
 {
 
+       cout <<"__LINE__ " <<__LINE__  <<endl;
+
+//    C *pc;
+//    pc = new C();
+//    delete pc;
     return 0;
 }
 
