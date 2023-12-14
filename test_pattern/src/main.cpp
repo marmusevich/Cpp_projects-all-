fa48e8f0fba131a0231204d3d2bf0d082ce2a9fd
marmusevich <myrmik@gmail.com>
2023-12-14 13:23:03 +0200

    clean up

--- test_pattern/src/main.cpp
+++ test_pattern/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include <conio.h> //getch() - windos only - todo get rid
+
 //fwd
 void visitor_test();
 void decorator_test();
@@ -15,5 +17,9 @@ int main()
 
 
 
+	//------------------------
+	std::cout <<"\n\nEnd - press any key\n";
+	//windos only - todo get rid
+	(void)getch();
 }
 
