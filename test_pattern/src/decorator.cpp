f4f24081335b358225210123f8f13b6d25b27255
marmusevich <myrmik@gmail.com>
2023-12-14 21:14:06 +0200

    + decorator

--- test_pattern/src/decorator.cpp
+++ test_pattern/src/decorator.cpp
@@ -1,15 +1,104 @@
 #include <iostream>
-
+#include <vector>
+#include <functional>
 
 namespace
 {
 	//fwd
 
+	struct IServer
+	{
+		virtual void doTask() const noexcept = 0;
+
+		virtual ~IServer() = default;
+		IServer() = default;
+	};
+
+	struct CServer : public IServer
+	{
+		void doTask() const noexcept override
+		{
+			std::cout << "\t\t\tMake work concrete server\n";
+		}
+	};
+
+	struct IExtendServer : public IServer
+	{
+		IExtendServer(IServer const& parent)
+			: mParent(parent)
+		{
+
+		}
+
+	protected:
+		std::reference_wrapper<const IServer> mParent;
+	};
+
+	struct CLogServer : public IExtendServer
+	{
+		CLogServer(IServer const& parent)
+			: IExtendServer(parent)
+		{
+
+		}
+
+		void doTask() const noexcept override
+		{
+			std::cout << "\t\t>>> log work of server before\n";
+			mParent.get().doTask();
+			std::cout << "\t\t>>> log work of server after\n";
+		}
+	};
+
+
+	struct CMeasureServer : public IExtendServer
+	{
+		CMeasureServer(IServer const& parent)
+			: IExtendServer(parent)
+		{
+
+		}
+
+		void doTask() const noexcept override
+		{
+			std::cout << "\t--- measure work of server before\n";
+			mParent.get().doTask();
+			std::cout << "\t--- measure work of server after\n";
+		}
+	};
 }
 
 
 void decorator_test()
 {
 
+	CServer server;
+
+	{
+		std::cout << "clear server: \n";
+		server.doTask();
+		std::cout << " ...\n\n";
+	}
+
+	{
+		auto decarated = CLogServer{ server };
+		std::cout << "logging server: \n";
+		decarated.doTask();
+		std::cout << " ...\n\n";
+	}
+
+	{
+		auto decarated = CLogServer{ CMeasureServer{server } };
+		std::cout << "logging + measure server: \n";
+		decarated.doTask();
+		std::cout << " ...\n\n";
+	}
+
+	{
+		auto decarated = CMeasureServer{ CLogServer{server } };
+		std::cout << "measure logging server: \n";
+		decarated.doTask();
+		std::cout << " ...\n\n";
+	}
 }
 
--- test_pattern/src/templMethod.cpp
+++ test_pattern/src/templMethod.cpp
@@ -4,108 +4,108 @@
 
 namespace
 {
-   class ADificultCalc
-   {
-   public:
-      ADificultCalc() = default;
-      virtual ~ADificultCalc() = default;
+	class ADificultCalc
+	{
+	public:
+		ADificultCalc() = default;
+		virtual ~ADificultCalc() = default;
 
-      void calc()
-      {
-         p1();
-         m1();
-         p2();
-         m2();
-         p3();
-         m3();
-         p4();
-      }
+		void calc() const noexcept
+		{
+			p1();
+			m1();
+			p2();
+			m2();
+			p3();
+			m3();
+			p4();
+		}
 
-   protected:
-      virtual void m1() = 0;
-      virtual void m2() = 0;
-      virtual void m3() = 0
-      {
-         std::cout <<" ---  m3 - default\n";
-      }
+	protected:
+		virtual void m1() const noexcept = 0;
+		virtual void m2() const noexcept = 0;
+		virtual void m3() const noexcept = 0
+		{
+			std::cout << " ---  m3 - default\n";
+		}
 
-   private:
-      void p1()
-      {
-         std::cout <<"part 1\n";
-      }
+	private:
+		void p1() const noexcept
+		{
+			std::cout << "part 1\n";
+		}
 
-      void p2()
-      {
-         std::cout <<"part 2\n";
-      }
+		void p2() const noexcept
+		{
+			std::cout << "part 2\n";
+		}
 
-      void p3()
-      {
-         std::cout <<"part 3\n";
-      }
+		void p3() const noexcept
+		{
+			std::cout << "part 3\n";
+		}
 
-      void p4()
-      {
-         std::cout <<"part 4\n";
-      }
-   };
+		void p4() const noexcept
+		{
+			std::cout << "part 4\n";
+		}
+	};
 
-   class cConcreteCalc1 : public ADificultCalc
-   {
-   protected:
-      void m1() override
-      {
-         std::cout << "   m1 - variant 1\n";
-      }
+	class cConcreteCalc1 : public ADificultCalc
+	{
+	protected:
+		void m1() const noexcept override
+		{
+			std::cout << "   m1 - variant 1\n";
+		}
 
-      void m2() override
-      {
-         std::cout << "   m2 - variant 1\n";
-      }
+		void m2() const noexcept override
+		{
+			std::cout << "   m2 - variant 1\n";
+		}
 
-      void m3() override
-      {
-         std::cout << "   m3 - variant 1\n";
-      }
-   };
+		void m3() const noexcept override
+		{
+			std::cout << "   m3 - variant 1\n";
+		}
+	};
 
-   class cConcreteCalc2 : public ADificultCalc
-   {
-   protected:
-      void m1() override
-      {
-         std::cout << "        m1 - variant 2 !!!! \n";
-      }
+	class cConcreteCalc2 : public ADificultCalc
+	{
+	protected:
+		void m1() const noexcept override
+		{
+			std::cout << "        m1 - variant 2 !!!! \n";
+		}
 
-      void m2() override
-      {
-         //do nothing
-      }
+		void m2() const noexcept override
+		{
+			//do nothing
+		}
 
-      void m3() override
-      {
-         //use default impl
-         ADificultCalc::m3();
-      }
+		void m3() const noexcept override
+		{
+			//use default impl
+			ADificultCalc::m3();
+		}
 
-   private:
-      void p1()
-      {
-         std::cout <<"ERROR MUST NOT CALL  cConcreteCalc2::p1\n";
-      }
-   };
+	private:
+		void p1() const noexcept
+		{
+			std::cout << "ERROR MUST NOT CALL  cConcreteCalc2::p1\n";
+		}
+	};
 }
 
 
 void templMethod_test()
 {
-   auto c1 = std::make_shared<cConcreteCalc1>();
-   auto c2 = std::make_shared<cConcreteCalc2>();
+	auto c1 = std::make_shared<cConcreteCalc1>();
+	auto c2 = std::make_shared<cConcreteCalc2>();
 
-   std::cout << "*** variant   <I> \n";
-   c1->calc();
-   std::cout << "*** variant   <II> \n";
-   c2->calc();
+	std::cout << "*** variant   <I> \n";
+	c1->calc();
+	std::cout << "*** variant   <II> \n";
+	c2->calc();
 }
 
