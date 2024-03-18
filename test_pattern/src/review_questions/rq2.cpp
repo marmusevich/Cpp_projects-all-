32199eb8fa526083932b8860abae6914dc30a065
marmusevich <myrmik@gmail.com>
2024-03-18 13:30:58 +0200

    +

--- test_pattern/src/review_questions/rq2.cpp
+++ test_pattern/src/review_questions/rq2.cpp
@@ -813,13 +813,11 @@ namespace
 		void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 		{
 			int i = m + n - 1;
-			int i1 = m  - 1;
+			int i1 = m - 1;
 			int i2 = n - 1;
 
 			while (i >= 0 && (i1 >= 0 || i2 >= 0))
 			{
-				printf("i = %d \ti1 = %d \ti2 = %d \n", i, i1, i2);
-
 				if (i2 < 0)
 				{
 					nums1[i] = nums1[i1];
