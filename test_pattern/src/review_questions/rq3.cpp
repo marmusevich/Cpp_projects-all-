6d64d3d6d5de6efda23a7311366359044fecc799
marmusevich <myrmik@gmail.com>
2024-06-12 16:16:18 +0300

    + last

--- test_pattern/src/review_questions/rq3.cpp
+++ test_pattern/src/review_questions/rq3.cpp
@@ -25,7 +25,7 @@ namespace
 	class Solution_27
 	{
 	public:
-		int removeElement_(std::vector<int>& nums, int val)
+		int removeElement(std::vector<int>& nums, int val)
 		{
 			if (nums.empty()) // special case - empty 
 			{
@@ -49,13 +49,6 @@ namespace
 
 			while (it_read != nums.end() && it_write != nums.end())
 			{
-				//printf("it_read [%d] = %d \tit_write [%d] = %d \t\t c = %d \n", 
-				//	std::distance(nums.begin(), it_read) , *it_read,
-				//	std::distance(nums.begin(), it_write), *it_write,
-				//	c);
-
-
-
 				if (*it_write != val)
 				{
 					it_write++;
@@ -75,78 +68,9 @@ namespace
 
 			}
 
-			//printf("---it_read [%d] = %d \tit_write [%d] = %d \t\t c = %d\n",
-			//	(it_read != nums.end() ? std::distance(nums.begin() , it_read) : -1), (it_read != nums.end() ? *it_read : -1),
-			//	(it_write != nums.end() ? std::distance(nums.begin(), it_write) : -1), (it_write != nums.end() ? *it_write : -1),
-			//	c);
-
 			return c;
-			//return std::distance(nums.begin(), it_write);
 		}
 
-		int removeElement(std::vector<int>& nums, int val)
-		{
-			if (nums.empty()) // special case - empty 
-			{
-				return 0;
-			}
-
-			if (nums.size() == 1)// special case - only one element
-			{
-				if (*(nums.begin()) == val)//end we must delete it
-				{
-					return 0;
-				}
-				return 1;//or not
-			}
-
-			auto it_write = nums.begin();
-			auto it_read = std::prev(nums.end());
-
-			int c = 0;
-
-			while (it_read > it_write)
-			{
-				//printf("it_read [%d] = %d \tit_write [%d] = %d \t\t c = %d \n", 
-				//	std::distance(nums.begin(), it_read) , *it_read,
-				//	std::distance(nums.begin(), it_write), *it_write,
-				//	c);
-
-
-
-				if (*it_write != val)
-				{
-					it_write++;
-					c++;
-				}
-				else
-				{
-					if (it_read > it_write && *it_read != val)
-					{
-						std::swap(*it_write, *it_read);
-						it_write++;
-						it_read--;
-						c++;
-					}
-				}
-
-				if (*it_read == val)
-				{
-					it_read--;
-				}
-
-
-
-			}
-
-			//printf("---it_read [%d] = %d \tit_write [%d] = %d \t\t c = %d\n",
-			//	(it_read != nums.end() ? std::distance(nums.begin() , it_read) : -1), (it_read != nums.end() ? *it_read : -1),
-			//	(it_write != nums.end() ? std::distance(nums.begin(), it_write) : -1), (it_write != nums.end() ? *it_write : -1),
-			//	c);
-
-			return c;
-			//return std::distance(nums.begin(), it_write);
-		}
 
 		static void test()
 		{
